a0501099ac27ba63fb91a4400e94b044ecb49cf5
dacaiguoguo <dacaiguoguo@163.com>
2024-04-09 10:32:37 +0800

    可以水平移动了，差对齐分割线了

--- addLine.sh
+++ addLine.sh
@@ -7,7 +7,7 @@
 
 ffmpeg -y \
 -i background.mp4 \
--loop 1 -i overlayH.png \
--filter_complex "[1:v]loop=-1:size=1,setpts=N/FRAME_RATE/TB[v1];[0:v][v1]overlay=y='t*(H-h)':shortest=1" \
+-loop 1 -i overlay.png \
+-filter_complex "[1:v]loop=-1:size=1,setpts=N/FRAME_RATE/TB[v1];[0:v][v1]overlay=x='t*(W-w)':shortest=1" \
 -t 4 \
-addLineOutputH.mp4
+addLineOutputH2.mp4
